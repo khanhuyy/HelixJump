ee38852e665717ae19599f63f52209bf654821e1
khanhuy <huypk239@gmail.com>
2024-05-10 18:31:16 +0700

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float rotationSpeed = 200.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+    }
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.name == "Sphere") {
+            Debug.Log("collsion");
+        }
+    }
+}
