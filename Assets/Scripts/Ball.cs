a62fafc60ba514917bd9cf11a786851fc589ae1a
khanhhuypham <huypk239@gmail.com>
2025-12-30 17:38:18 +0700

    migrate 6000

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -45,13 +45,13 @@ public class Ball : MonoBehaviour
     }
 
     private void LateUpdate() {
-        if (rb.velocity.y <= MAX_FALL_SPEED) {
-            rb.velocity = new Vector3(0, MAX_FALL_SPEED, 0);
+        if (rb.linearVelocity.y <= MAX_FALL_SPEED) {
+            rb.linearVelocity = new Vector3(0, MAX_FALL_SPEED, 0);
         }
     }
 
     private void OnCollisionEnter(Collision other) {
-        rb.velocity = new Vector3(0, bouncingSpeed, 0) * Time.deltaTime;
+        rb.linearVelocity = new Vector3(0, bouncingSpeed, 0) * Time.deltaTime;
         GameObject newSplit = Instantiate(splitPrefab, new Vector3(transform.position.x, other.transform.position.y + 0.19f, transform.position.z), transform.rotation);
         newSplit.transform.localScale = Vector3.one * Random.Range(0.3f, 0.5f);
         newSplit.transform.parent = other.transform;
